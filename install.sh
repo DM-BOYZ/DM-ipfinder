45a749fc6bba334f5a6e6cf4dab05e09e58c36f2
DM-BOYZ <71525581+DM-BOYZ@users.noreply.github.com>
2021-01-16 04:29:39 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+apt update
+
+apt install python -y
+apt install python2 -y
+apt install ruby -y
+apt install figlet -y
+gem install lolcat
+echo '
+╒══╩╩━╩╩═══════════╕
+🌾python3 iptracer.py 🌾
+╘══════════════════╛'
